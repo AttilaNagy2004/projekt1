0f0db13994cad7d5184aefbbaae37b977a6d50b9
AttilaNagy2004 <acus2004@gmail.com>
2021-12-13 22:31:32 +0100

    Bezárás után mentés (helyes változat)

--- Projekt1/Projekt1/MainWindow.xaml.cs
+++ Projekt1/Projekt1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Projekt1
 {
@@ -23,20 +24,7 @@ namespace Projekt1
         public MainWindow()
         {
             InitializeComponent();
-            if (System.IO.File.Exists("text.txt"))
-            {
-                string xaml = System.IO.File.ReadAllText("text.txt");
-                object content = System.Windows.Markup.XamlReader.Parse(xaml);
-                this.Content = content;
-            }
     }
-        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
-        {
-            base.OnClosing(e);
-            string xaml = System.Windows.Markup.XamlWriter.Save(this.Content);
-            System.IO.File.WriteAllText("text.txt", xaml);
-        }
-
 
         private void ujhozzaad_Click(object sender, RoutedEventArgs e)
         {
@@ -87,5 +75,52 @@ namespace Projekt1
             kuka.Items.Remove(delete);
         }
 
+        private void MainWindow1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            List<string> checkboxok = new List<string>();
+            foreach (CheckBox x in lista.Items)
+            {
+                int allapot = 0;
+                if (x.IsChecked == true)
+                    allapot = 1;
+                string cbJellemzoje = x.Content.ToString() + ";" + allapot;
+                checkboxok.Add(cbJellemzoje);
+            }
+            File.WriteAllLines("tarolo.txt", checkboxok);
+
+            List<string> checkbox = new List<string>();
+            foreach (CheckBox y in kuka.Items)
+            {
+                int allapot1 = 0;
+                if (y.IsChecked == true)
+                    allapot1 = 1;
+                string cbJellemzoje1 = y.Content.ToString() + ";" + allapot1;
+                checkbox.Add(cbJellemzoje1);
+            }
+            File.WriteAllLines("tarolo1.txt", checkbox);
+        }
+
+        private void MainWindow1_Activated(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+            var be = File.ReadAllLines("tarolo.txt");
+            foreach (var x in be)
+            {
+                CheckBox uj = new CheckBox();
+                uj.Content = x.Split(';')[0];
+                uj.IsChecked = x.Split(';')[1] == "1" ? true : false;
+                lista.Items.Add(uj);
+            }
+
+            kuka.Items.Clear();
+            var be1 = File.ReadAllLines("tarolo1.txt");
+            foreach (var y in be1)
+            {
+                CheckBox uj1 = new CheckBox();
+                uj1.Content = y.Split(';')[0];
+                uj1.IsChecked = y.Split(';')[1] == "1" ? true : false;
+                kuka.Items.Add(uj1);
+            }
+        }
     }
 }
